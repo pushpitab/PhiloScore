8505f92ff0035adc9faff847bf736d9bbe1e1979
pushpitab <pushpita@mit.edu>
2018-06-24 23:02:44 -0400

    login code

---
+++ src/auth.js
@@ -0,0 +1,53 @@
+import firebase from 'firebase'
+import firebaseui from 'firebaseui';
+
+const config = {
+  apiKey: "AIzaSyCPMlbHQq6zPm6YbDuJvLR4hj7SK4_VeWo",
+    authDomain: "philescore.firebaseapp.com",
+    databaseURL: "https://philescore.firebaseio.com",
+    projectId: "philescore",
+    storageBucket: "philescore.appspot.com",
+    messagingSenderId: "347007115689"
+};
+
+const auth = {
+  context: null,
+  uiConfig: null,
+  ui: null,
+
+  init(context) {
+    this.context = context;
+
+    firebase.initializeApp(config);
+    this.uiConfig = {
+      signInSuccessUrl: 'dashboard',
+      signInOptions: [
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ]
+    }
+    this.ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+    firebase.auth().onAuthStateChanged((user) => {
+      this.context.$store.dispatch('user/setCurrentUser')
+
+      let requireAuth = this.context.$route.matched.some(record => record.meta.requireAuth)
+      let guestOnly = this.context.$route.matched.some(record => record.meta.guestOnly)
+
+      if(requireAuth && !user) this.context.$router.push('auth')
+      else if (guestOnly && user) this.context.$router.push('dashboard')
+    });
+  },
+  authForm(container) {
+    this.ui.start(container, this.uiConfig);
+  },
+  user() {
+    return this.context ? firebase.auth().currentUser : null;
+  },
+  logout() {
+    firebase.auth().signOut();
+  }
+}
+
+export default auth;
